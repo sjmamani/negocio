2192e9fe8495c41c9087180edf7987bb707bc0d9
Santiago, Mamani <santiagojmamani@gmail.com>
2019-06-06 22:16:54 -0300

    ModificarProducto modifica solo el precio

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -32,161 +32,185 @@ import view.RubroView;
 import view.SubRubroView;
 
 public class Controlador {
-	
+
 	private static Controlador instancia;
-	
-	private Controlador(){ }
-	
-	public static Controlador getInstancia(){
-		if(instancia == null)
+
+	private Controlador() {
+	}
+
+	public static Controlador getInstancia() {
+		if (instancia == null)
 			instancia = new Controlador();
 		return instancia;
 	}
 
 	/**
-	 * Verifica el login y el password ingresado, si son incorrecto lanza un LoginException
+	 * Verifica el login y el password ingresado, si son incorrecto lanza un
+	 * LoginException
 	 * 
-	 * Si el password tiene mas de x días lanza un CambioPasswordException indicando que la password está vencida
-	 * @throws UsuarioException 
+	 * Si el password tiene mas de x días lanza un CambioPasswordException indicando
+	 * que la password está vencida
 	 * 
-	 * */
-	public boolean login(String nombre, String password) throws LoginException, CambioPasswordException, UsuarioException{
+	 * @throws UsuarioException
+	 * 
+	 */
+	public boolean login(String nombre, String password)
+			throws LoginException, CambioPasswordException, UsuarioException {
 		Usuario usuario = UsuarioDAO.getInstancia().getUsuarioByNombre(nombre);
-		if(usuario.getPassword().equals(password)){
-			if(usuario.debeCambiar()) {
+		if (usuario.getPassword().equals(password)) {
+			if (usuario.debeCambiar()) {
 				throw new CambioPasswordException("La password esta vencida, debe cambiarla");
 			}
 			return true;
-		}
-		else{
+		} else {
 			throw new LoginException("Los datos ingresado no son corrector, reingrese");
 		}
 	}
+
 	/**
 	 * Permite cambiar el password de un usuario existente.
 	 * 
 	 * El password debe serguir las reglas de formacion y frecuencia.
 	 * 
 	 * 
-	 * */
-	/*Probado*/
-	public void cambioPassword(String nombre, String password) throws CambioPasswordException, UsuarioException{
+	 */
+	/* Probado */
+	public void cambioPassword(String nombre, String password) throws CambioPasswordException, UsuarioException {
 		Usuario usuario = UsuarioDAO.getInstancia().getUsuarioByNombre(nombre);
 		usuario.actualizoPassword(password);
 	}
-	/*Probado*/
-	public ProductoView altaProducto(ProductoView recibido) throws RubroException, SubRubroException{
+
+	/* Probado */
+	public ProductoView altaProducto(ProductoView recibido) throws RubroException, SubRubroException {
 		Rubro auxR = RubroDao.getInstancia().findByCodigo(recibido.getRubro().getCodigo());
 		SubRubro auxSR = SubRubroDao.getInstancia().findByCodigo(recibido.getSubRubro().getCodigo());
-		Producto producto = new Producto(auxSR,auxR,recibido.getNombre(), recibido.getMarca(), recibido.getCodigoBarras(), recibido.getPrecio());
+		Producto producto = new Producto(auxSR, auxR, recibido.getNombre(), recibido.getMarca(),
+				recibido.getCodigoBarras(), recibido.getPrecio());
 		producto.save();
 		return producto.toView();
 	}
-	/*Probado*/
-	public void bajaProducto(ProductoView recibido) throws ProductoException{
+
+	/* Probado */
+	public void bajaProducto(ProductoView recibido) throws ProductoException {
 		Producto producto = ProductoDAO.getInstancia().findProductoByIdentificador(recibido.getIdentificador());
 		producto.delete();
 		producto = null;
 	}
-	/*Probado*/
-	public void modificaProducto(ProductoView recibido) throws ProductoException{
+
+	/* Probado */
+	public void modificaProducto(ProductoView recibido) throws ProductoException {
 		Producto producto = ProductoDAO.getInstancia().findProductoByIdentificador(recibido.getIdentificador());
+		producto.setPrecio(recibido.getPrecio());
 		producto.update();
 	}
-	/*Probado*/
-	public PedidoView crearPedido(PedidoView pedido) throws ClienteException{
+
+	/* Probado */
+	public PedidoView crearPedido(PedidoView pedido) throws ClienteException {
 		Cliente cliente = ClienteDAO.getInstancia().findClienteByCuit(pedido.getCliente().getCuil());
 		Pedido nuevoPedido = new Pedido(cliente);
 		nuevoPedido.save();
 		return nuevoPedido.toView();
 	}
-	/*Probado*/
+
+	/* Probado */
 	public PedidoView crearPedido(String cuit) throws ClienteException {
 		Cliente cliente = ClienteDAO.getInstancia().findClienteByCuit(cuit);
 		Pedido nuevoPedido = new Pedido(cliente);
 		nuevoPedido.save();
-		return nuevoPedido.toView();	
+		return nuevoPedido.toView();
 	}
-	/*Probado*/
-	public ItemPedidoView agregarProductoEnPedido(int numeroPedido, int identificadorProducto, int cantidad) throws PedidoException, ProductoException{
+
+	/* Probado */
+	public ItemPedidoView agregarProductoEnPedido(int numeroPedido, int identificadorProducto, int cantidad)
+			throws PedidoException, ProductoException {
 		Pedido pedido = PedidoDAO.getInstancia().findPedidoByNumero(numeroPedido);
 		Producto producto = ProductoDAO.getInstancia().findProductoByIdentificador(identificadorProducto);
 		ItemPedido item = pedido.addProductoEnPedido(producto, cantidad);
 		return item.toView();
 	}
-	/*Probado*/
-	public void eliminarPedido(int numeroPedido){
+
+	/* Probado */
+	public void eliminarPedido(int numeroPedido) {
 		PedidoDAO.getInstancia().delete(numeroPedido);
 	}
-	/*Probado*/
-	public void facturarPedido(int numero) throws PedidoException{
+
+	/* Probado */
+	public void facturarPedido(int numero) throws PedidoException {
 		PedidoDAO.getInstancia().findPedidoByNumero(numero).facturar();
 	}
-	/*Probado*/
-	public PedidoView getPedidoById(int numero) throws PedidoException{
+
+	/* Probado */
+	public PedidoView getPedidoById(int numero) throws PedidoException {
 		return PedidoDAO.getInstancia().findPedidoByNumero(numero).toView();
 	}
+
 	public List<PedidoView> getPedidos() {
 		List<PedidoView> resultado = new ArrayList<PedidoView>();
-		for(Pedido p : PedidoDAO.getInstancia().findAll())
-				resultado.add(p.toView());
+		for (Pedido p : PedidoDAO.getInstancia().findAll())
+			resultado.add(p.toView());
 		return resultado;
 	}
-	/*Probado*/	
-	public List<RubroView> getRubros(){
+
+	/* Probado */
+	public List<RubroView> getRubros() {
 		List<RubroView> resultado = new ArrayList<RubroView>();
-		for(Rubro r : RubroDao.getInstancia().findAll())
+		for (Rubro r : RubroDao.getInstancia().findAll())
 			resultado.add(r.toView());
 		return resultado;
 	}
-	/*Probado*/
-	public List<SubRubroView> getSubRubros(){
+
+	/* Probado */
+	public List<SubRubroView> getSubRubros() {
 		List<SubRubroView> resultado = new ArrayList<SubRubroView>();
-		for(SubRubro r : SubRubroDao.getInstancia().findAll())
+		for (SubRubro r : SubRubroDao.getInstancia().findAll())
 			resultado.add(r.toView());
 		return resultado;
 	}
-	/*Probado*/
-	public List<ProductoView> getProductos(){
+
+	/* Probado */
+	public List<ProductoView> getProductos() {
 		List<ProductoView> resultado = new ArrayList<ProductoView>();
 		List<Producto> productos = ProductoDAO.getInstancia().findAll();
-		for(Producto producto : productos)
+		for (Producto producto : productos)
 			resultado.add(producto.toView());
 		return resultado;
 	}
-	/*Probado*/
-	public List<ProductoView> getProductosByRubro(RubroView rubro){
+
+	/* Probado */
+	public List<ProductoView> getProductosByRubro(RubroView rubro) {
 		List<ProductoView> resultado = new ArrayList<ProductoView>();
 		List<Producto> productos = ProductoDAO.getInstancia().findProductoByRubro(rubro.getCodigo());
-		for(Producto producto : productos)
+		for (Producto producto : productos)
 			resultado.add(producto.toView());
 		return resultado;
 	}
-	/*Probado*/
-	public List<ProductoView> getProductosBySubRubro(SubRubroView subRubro){
+
+	/* Probado */
+	public List<ProductoView> getProductosBySubRubro(SubRubroView subRubro) {
 		List<ProductoView> resultado = new ArrayList<ProductoView>();
 		List<Producto> productos = ProductoDAO.getInstancia().findProductoBySubRubro(subRubro.getCodigo());
-		for(Producto producto : productos)
+		for (Producto producto : productos)
 			resultado.add(producto.toView());
 		return resultado;
 	}
-	/*Probado*/
-	public List<ClienteView> getClientes(){
+
+	/* Probado */
+	public List<ClienteView> getClientes() {
 		List<ClienteView> resultado = new ArrayList<ClienteView>();
 		List<Cliente> clientes = ClienteDAO.getInstancia().findAll();
-		for(Cliente cliente : clientes)
+		for (Cliente cliente : clientes)
 			resultado.add(cliente.toView());
 		return resultado;
 	}
+
 	public ProductoView getProductoById(int id) throws ProductoException {
 		return ProductoDAO.getInstancia().findProductoByIdentificador(id).toView();
 	}
 
 	public void altaUsuario(String nombre, String password) throws CambioPasswordException, UsuarioException {
 		// TODO Auto-generated method stub
-		
-		new Usuario(nombre,password).save();
-		
-			
+
+		new Usuario(nombre, password).save();
+
 	}
 }
